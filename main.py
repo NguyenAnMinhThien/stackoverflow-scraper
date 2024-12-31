71d904ed24115cace8ef2806b5d26bcb1995eaf0
MinhThien <nguyenanminhthien97@gmail.com>
2024-12-31 15:45:41 +0700

    Add multiple tags choice, venv

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import utils
+
+
+def welcome():
+    print("Welcome.")
+    print("What tag name do you want to extract from Stackoverflow ? e.x 'windows' or 'windows javascript'")
+    tag = input()
+    print("The number of pages ? (each page contains 50 results)")
+    pages = input()
+
+    return tag, pages
+
+
+if __name__ == '__main__':
+    tag, pages = welcome()
+    # Get all the questions follow up the number of votes
+    my_df = utils.extract_all_page(tag=tag, page_nums=pages)
+    # Save file
+
+
+    filename, filepath = utils.get_file_name(tag, pages)
+    my_df.to_csv(filename)
+    print(my_df)
+    print(f"The file has been saved at: \n{filepath}")
---
+++ utils.py
@@ -0,0 +1,81 @@
+import re
+import requests
+import sys
+from bs4 import BeautifulSoup
+import pandas
+import os
+
+session = requests.Session()
+
+
+def get_url(tag,  page_num):
+    tags = tag.split(" ")
+    tags = [x.strip() for x in tags]
+    if tags.__len__() > 1:
+        search_tag = str.join("%2b",tags)
+    else:
+        search_tag = tags[0]
+    url = f"https://stackoverflow.com/questions/tagged/{search_tag}?tab=votes&page={page_num}&pagesize=50"
+    return url
+
+
+def load_page(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US, en;q=0.5'}
+    try:
+        r = session.get(url=url, headers=headers)
+        if r.status_code == 200:
+            return r
+    except Exception as e:
+        print("Something wrong.", e)
+
+
+
+def extract_page(tag,  page_num):
+    url = get_url(tag,  page_num)
+    r = load_page(url)
+
+
+    try:
+        soup = BeautifulSoup(r.content, features="html.parser")
+        my_df = pandas.DataFrame(columns=['Votes', 'Title', 'Summary'])
+        contents = soup.find_all("div", class_="s-post-summary js-post-summary")
+        if contents == []:
+            sys.exit(f"Please check again your tag name. The tag '{tag}' is not exist.\nCheck the following URL:\nhttps://stackoverflow.com/tags")
+        for content in contents:
+            vote = (content.find("span", class_="s-post-summary--stats-item-number").get_text().strip())
+            title = (content.find("h3", class_="s-post-summary--content-title").get_text().strip())
+            summary = (content.find("div", class_="s-post-summary--content-excerpt").get_text().strip())
+            if vote == "" or title == "" or summary == "":
+                sys.exit(f"The page html has change element structure.")
+            dftemp = pandas.DataFrame({'Votes': [vote], 'Title': [title], 'Summary': [summary]})
+            my_df = my_df._append(dftemp, ignore_index=True)
+    except Exception as e:
+        print("Something wrong: ", e)
+    else:
+        return my_df
+
+
+def extract_all_page(tag,  page_nums):
+    my_df = pandas.DataFrame()
+    for page in range(1, int(page_nums) + 1):
+        df = extract_page(tag=tag,  page_num=page)
+        my_df = my_df._append(df, ignore_index=True)
+    return my_df
+
+def get_file_name(tag, page_num):
+    tags = tag.split(" ")
+    tags = [x.strip() for x in tags]
+    if tags.__len__() > 1:
+        search_tag = str.join("-", tags)
+    else:
+        search_tag = tags[0]
+    filename = f"{search_tag}-{page_num}p.csv"
+    if os.name == "nt":
+        # window
+        filepath = os.getcwd() + "\\" + filename
+    else:
+        # other
+        filepath = os.getcwd() + "/" + filename
+    return filename, filepath
